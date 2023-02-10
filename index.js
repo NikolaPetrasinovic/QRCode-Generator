1b6786dfc154ddcd0faf05e475c78189a4d035ee
NikolaPetrasinovic <nikola.petrasinovic@dev.telnet.rs>
2023-02-10 14:21:46 +0100

    izmene

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ qrImg = wrapper.querySelector(".qr-code img");
 
 generateBtn.addEventListener("click", () => {
     let qrValue = qrInput.value;
-    if(!qrValue) return;
+    if(!qrValue) return; //ako je prazno vraca ovde
     generateBtn.innerText = "Generating QR Code"
     qrImg.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example=${qrValue}`;
     qrImg.addEventListener("load", () => {
